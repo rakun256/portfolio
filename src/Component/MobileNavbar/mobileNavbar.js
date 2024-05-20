9ff89d9572d93e1205e02da9e8e271a45abf0780
Emre Uslu <emreuslu256@gmail.com>
2024-05-20 22:11:20 +0300

    deneme

--- src/Component/MobileNavbar/mobileNavbar.js
+++ src/Component/MobileNavbar/mobileNavbar.js
@@ -1,43 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./mobileNavbar.css";
-import { Link } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const MobileNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
 
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    setIsOpen(false); // URL değiştiğinde navbar'ı kapat
+  }, [location]);
 
   return (
     <div className="mobile-navbar-container">
       <div className="navbar-name">
         <h1>Emre Uslu</h1>
       </div>
-      
-        <div className="mobile-navbar-toggle" onClick={toggleNavbar}>
-          <i className={isOpen ? "fas fa-times" : "fas fa-bars"}></i>
-        </div>
-        <div className={`mobile-navbar-links ${isOpen ? "open" : ""}`}>
-          <Link to={`/`}>
-            <span className="navbar-links-text">Main Page</span>
-          </Link>
-          <Link to={`/about`}>
-            <span className="navbar-links-text">About Me</span>
-          </Link>
-          <Link to={`/projects`}>
-            <span className="navbar-links-text">Projects</span>
-          </Link>
-          <Link to={`/skills`}>
-            <span className="navbar-links-text">Skills</span>
-          </Link>
-          <Link to={`/contact`}>
-            <span className="navbar-links-text">Contact Me</span>
-          </Link>
-        </div>
+      <div className="mobile-navbar-toggle" onClick={toggleNavbar}>
+        <i className={isOpen ? "fas fa-times" : "fas fa-bars"}></i>
       </div>
-    
+      <div className={`mobile-navbar-links ${isOpen ? "open" : ""}`}>
+        <Link to={`/`}>
+          <span className="navbar-links-text">Main Page</span>
+        </Link>
+        <Link to={`/about`}>
+          <span className="navbar-links-text">About Me</span>
+        </Link>
+        <Link to={`/projects`}>
+          <span className="navbar-links-text">Projects</span>
+        </Link>
+        <Link to={`/skills`}>
+          <span className="navbar-links-text">Skills</span>
+        </Link>
+        <Link to={`/contact`}>
+          <span className="navbar-links-text">Contact Me</span>
+        </Link>
+      </div>
+    </div>
   );
 };
 
