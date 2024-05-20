cf7ae4d2763cd01b19cb7651cffd9dc06bf2723f
Emre Uslu <emreuslu256@gmail.com>
2024-05-20 20:52:16 +0300

    MainPage güzelleştirildi.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Routes, Route, BrowserRouter, useParams } from "react-router-dom";
 import Navbar from "./Component/Navbar/navbar";
-import MobileNavbar from "./Component/MobileNavbar/mobileNavbar"
+import MobileNavbar from "./Component/MobileNavbar/mobileNavbar";
 import MainPage from "./Pages/MainPage/mainPage";
 import AboutMe from "./Pages/AboutMe/aboutMe";
 import Projects from "./Pages/Projects/projects";
@@ -15,15 +15,16 @@ function App() {
         <div className="navbar-app">
           <Navbar />
         </div>
-        <div className="mobile-navbar-app"><MobileNavbar/></div>
-
+        <div className="mobile-navbar-app">
+          <MobileNavbar />
+        </div>
         <div className="app-renderer">
           <Routes>
-            <Route path="/" element={<MainPage />}/>
-            <Route path="/about" element={<AboutMe />}/>
-            <Route path="/projects" element={<Projects />}/>
-            <Route path="/skills" element={<Skills />}/>
-            <Route path="/contact" element={<Contact />}/>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
       </div>
--- src/Pages/MainPage/mainPage.js
+++ src/Pages/MainPage/mainPage.js
@@ -1,30 +1,40 @@
-import React from 'react'
-import './mainPage.css'
-import emre from '../../Images/emre.jpg'
+import React from "react";
+import "./mainPage.css";
+import emre from "../../Images/emre.jpg";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaRegEnvelope } from "react-icons/fa6";
+import { FaSquareGithub } from "react-icons/fa6";
 
 const MainPage = () => {
   return (
-    <div className='mainpage-container'>
-      <div className='mainpage-flexbox'>
-      <div className='mainpage-text'>
-      <div className='mainpage-title'>
-          <h1>Hi, I'm Emre Uslu!</h1>
+    <div className="mainpage-container">
+      <div className="mainpage-flexbox">
+        <div className="mainpage-text">
+          <div className="mainpage-title">
+            <h1>Hi, I'm Emre Uslu!</h1>
+          </div>
+          <div className="mainpage-desc">
+            <p>Junior Frontend Developer.</p>
+          </div>
+          <div className="mainpage-socials">
+            <a href="https://linkedin.com/in/mreuslu" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin />
+            </a>
+            <a href="https://github.com/rakun256/" target="_blank" rel="noopener noreferrer">
+              <FaSquareGithub />
+            </a>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=emreuslu256@gmail.com" target="_blank" rel="noopener noreferrer">
+              <FaRegEnvelope />
+            </a>
+          </div>
         </div>
-        <div className='mainpage-desc'>
-          <p>I'm an enthusiastic frontend web application developer with a strong passion for crafting engaging and user-friendly digital experiences. Currently, I am pursuing my studies at Yildiz Technical University, where I am honing my skills and expanding my knowledge in web development.</p>
+        <div className="mainpage-photo">
+          <img src={emre} alt="emre" />
         </div>
       </div>
-        <div className='mainpage-photo'>
-          <img src={emre} alt='emre'/>
-        </div>
-      </div>
-      
-        <div className='mainpage-socials'>
-            
-        </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
